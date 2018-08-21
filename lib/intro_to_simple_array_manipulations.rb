e7fba5607263315107b260321bf34feb73bdab9a
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-21 19:52:07 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,5 +27,5 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
-  list_of_esoteric_programming_languages.insert(another_esoteric_language)
+  list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
 end
