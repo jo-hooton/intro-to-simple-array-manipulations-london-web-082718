c54b9eb3980fee9f159ff10126294ead849b2864
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-21 19:56:56 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -37,3 +37,7 @@ end
 def using_flatten(private_colleges_in_newyork)
   private_colleges_in_newyork.flatten
 end
+
+def using_delete(instructors, no_offense_steven)
+  instructors.delete("Steven")
+end
